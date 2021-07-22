2af4d9640a3268badd47ea3f218a4a8b5d6555ba
tiagosatur <tiagosatur@gmail.com>
2021-07-22 12:31:52 -0300

    Create posts model, route (get, post) and controller

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
+app.use("/posts", postRoutes);
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
